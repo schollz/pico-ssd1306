5240db5a8fdf39c009e942f3b39bbba531f900cc
Zack Scholl <zack.scholl@gmail.com>
2024-01-01 20:36:58 -0800

    fix pinouts

--- main.cpp
+++ main.cpp
@@ -57,9 +57,9 @@ int main() {
   gpio_set_dir(25, GPIO_OUT);
   gpio_put(25, 1);*/
 
-  SSD1306 display(128, 64, spi0, /*baudrate*/ 8000 * 1000, /*mosi*/ 19,
-                  /*cs*/ 17,
-                  /*sclk*/ 18, /*reset*/ 16, /*dc*/ 20);
+  SSD1306 display(128, 64, spi0, /*baudrate*/ 8000 * 1000, /*mosi*/ 3,
+                  /*cs*/ 5,
+                  /*sclk*/ 2, /*reset*/ 6, /*dc*/ 4);
   display.init();
 
   //   const Animation& anim = animation_blink;
@@ -80,10 +80,12 @@ int main() {
 
   // INITIALIZE ADC
   // intialize aray of 128 values
-  int16_t arr[128];
+  int16_t arr[2][128];
   uint8_t arr_idx = 0;
-  for (int16_t i = 0; i < 128; i++) {
-    arr[i] = 0;
+  for (uint8_t j = 0; j < 2; j++) {
+    for (int16_t i = 0; i < 128; i++) {
+      arr[j][i] = 0;
+    }
   }
 
   adc_init();
@@ -92,44 +94,47 @@ int main() {
   adc_gpio_init(28);
 
   while (true) {
-    // switch to adc channel 0
-    adc_select_input(0);
-    // get adc value
-    uint16_t adc_val = adc_read();
-    // move all values in the array over by one
-    for (int16_t i = 0; i < 127; i++) {
-      arr[i] = arr[i + 1];
-    }
-    arr[127] = adc_val * 64 / 4096;
+    for (uint8_t j = 0; j < 2; j++) {
+      // switch to adc channel 0
+      adc_select_input(j);
+      // get adc value
+      uint16_t adc_val = adc_read();
+      // move all values in the array over by one
+      for (int16_t i = 0; i < 127; i++) {
+        arr[j][i] = arr[j][i + 1];
+      }
+      arr[j][127] = adc_val * 64 / 4096;
 
-    display.clear();
-    for (int16_t i = 1; i < 128; i++) {
-      int16_t x0 = i - 1;
-      int16_t y0 = arr[i - 1];
-      int16_t x1 = i;
-      int16_t y1 = arr[i];
-      // color all the pixels between two points (x0,y0) and (x1,y1)
-      // (inclusive)
-      // draw continuous line
-      if (x0 == x1) {
-        display.draw_pixel(x0, y0, SSD1306_COLOR_ON);
-      } else {
-        float m = (float)(y1 - y0) / (float)(x1 - x0);
-        float b = y0 - m * x0;
-        int8_t inc = 1;
-        if (y1 < y0) {
-          inc = -1;
+      display.clear();
+      for (int16_t i = 1; i < 128; i++) {
+        int16_t x0 = i - 1;
+        int16_t y0 = arr[j][i - 1];
+        int16_t x1 = i;
+        int16_t y1 = arr[j][i];
+        // color all the pixels between two points (x0,y0) and (x1,y1)
+        // (inclusive)
+        // draw continuous line
+        if (x0 == x1) {
+          display.draw_pixel(x0, y0, SSD1306_COLOR_ON);
+        } else {
+          float m = (float)(y1 - y0) / (float)(x1 - x0);
+          float b = y0 - m * x0;
+          int8_t inc = 1;
+          if (y1 < y0) {
+            inc = -1;
+          }
+          for (int16_t y = y0; y <= y1; y += inc) {
+            int16_t x = round((float)(y - b) / m);
+            display.draw_pixel(x, y, SSD1306_COLOR_ON);
+          }
+          display.draw_pixel(x1, y1, SSD1306_COLOR_ON);
+          display.draw_pixel(x0, y0, SSD1306_COLOR_ON);
         }
-        for (int16_t y = y0; y <= y1; y += inc) {
-          int16_t x = round((float)(y - b) / m);
-          display.draw_pixel(x, y, SSD1306_COLOR_ON);
-        }
-        display.draw_pixel(x1, y1, SSD1306_COLOR_ON);
-        display.draw_pixel(x0, y0, SSD1306_COLOR_ON);
       }
     }
+
     display.update();
-    sleep_ms(5);
+    sleep_ms(1);
   }
 
   while (true) {
